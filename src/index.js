a7760971c734d68675382cacb45b12affdc0f501
Lautaro Pintos <lautipintos8@gmail.com>
2023-02-13 14:14:59 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <ItemListContainer title="Welcome to Plug BA!"/>
+    <ItemListContainer title={"Welcome to Plug BA!"}/>
   </React.StrictMode>
 );
